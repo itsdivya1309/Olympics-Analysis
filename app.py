2f3f970a4b978da3c3c7eb3e40e0f6ab39fb8ba2
Divya Tiwari <gungun130903@gmail.com>
2024-08-11 00:13:25 +0530

    Overall analysis

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 import preprocessor, helper
 
 medals = pd.read_csv('archive/Olympic_Games_Medal_Tally.csv')
+games = pd.read_csv('archive/Olympics_Games.csv')
+results = pd.read_csv('archive/Olympic_Results.csv')
+athletes = pd.read_csv('archive/Olympic_Athlete_Event_Results.csv')
 
 df = preprocessor.preprocess(medals)
+games = preprocessor.preprocess(games).loc[games['competition_date'] != '—']
+results = preprocessor.preprocess(results)
+athletes = preprocessor.preprocess(athletes)
 
 st.sidebar.title('Summer Olympics Analysis')
 user_menu = st.sidebar.radio(
@@ -40,3 +47,51 @@ if user_menu == 'Medal Tally':
         st.title(selected_country + ' performance in '+str(selected_year)+ ' Olympics')
 
     st.table(medal_tally)
+
+if user_menu == 'Overall Analysis':
+
+    editions = df['year'].nunique()
+    cities = games['city'].nunique()
+    sports = results['sport'].nunique()
+    events = results['event_title'].nunique()
+    nations = df['country'].nunique()
+    n_athletes = athletes['athlete'].nunique()
+
+
+    st.title('Quick Statistics')
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader(editions)
+        st.header('Editions')    
+    with col2:
+        st.subheader(cities)
+        st.header('Host cities')    
+    with col3:
+        st.subheader(sports)
+        st.header('Sports')
+
+    st.divider()      
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader(events)
+        st.header('Events')   
+    with col2:
+        st.subheader(nations)
+        st.header('Participating Nations')   
+    with col3:
+        st.subheader(n_athletes)
+        st.header('Total Athletes')
+
+    st.divider()
+
+    st.title('Participating Nations over the Years')
+    nations_over_time = helper.participating_nations_over_time(df)
+    fig = px.line(nations_over_time, x='Edition', y='No of Countries')
+    st.plotly_chart(fig)
+
+    st.title('No of Events over the Years')
+    events_over_years = helper.events_over_time(games, results)
+    fig = px.line(events_over_years, x='Edition', y='No of Sports')
+    st.plotly_chart(fig)
--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def medal_tally(medals):
     country_medals = medals.groupby('country').sum()[['gold','silver','bronze','total']].sort_values(
         ['total', 'gold','silver','bronze'],ascending=False).reset_index()
@@ -42,6 +44,17 @@ def fetch_medal_tally(medals,year, country):
             'year', ascending=False).reset_index()
     else:
         medal_tally = medal_tally.groupby('country').sum()[['gold','silver','bronze','total']].sort_values(
-        ['total', 'gold','silver','bronze'],ascending=False).reset_index()
+        ['gold','silver','bronze', 'total'],ascending=False).reset_index()
         
     return medal_tally
+
+def participating_nations_over_time(df):
+    nations_over_time = pd.DataFrame(df.groupby('year')['country'].nunique()).reset_index()
+    nations_over_time.rename(columns={'year': 'Edition', 'country': 'No of Countries'}, inplace=True)
+    return nations_over_time
+
+def events_over_time(games, results):
+    summer_events = pd.merge(results, games, on='edition_id')
+    events_over_time = pd.DataFrame(summer_events.groupby('year')['sport'].nunique()).reset_index()
+    events_over_time.rename(columns={'year':'Edition', 'sport':'No of Sports'}, inplace=True)
+    return events_over_time
